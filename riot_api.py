ff129bd8394a556f237cc5ee16c40aa010576009
Jason <jhur008@ucr.edu>
2020-05-31 19:13:57 -0700

    Overall works. Keyerror at random occurences(10th iteration, 30th, 80th, etc.)

--- riot_api.py
+++ riot_api.py
@@ -12,8 +12,6 @@ def requestSummonerMastery(region, summonerID):
     response = requests.get(URL)
     return response.json()
 
-#def request other stuff
-
 def requestMatchList(region, accountID):
     URL = 'https://' + region + '.api.riotgames.com/lol/match/v4/matchlists/by-account/' + accountID + '?api_key=' + api_key
     response = requests.get(URL)
@@ -22,94 +20,89 @@ def requestMatchList(region, accountID):
 def requestMatchInfo(region, matchID):
     URL = 'https://' + region + '.api.riotgames.com/lol/match/v4/matches/' + matchID + '?api_key=' + api_key
     response = requests.get(URL)
+    #if 'URL' not in response
     return response.json()
-   
 
 
+# get region and user info
 region = (str)(input('Type in a region: '))
 summonerName = (str)(input('Type in a summoner name: '))
 
+# find information about user from in-game(public) name
 name_JSON = requestSummonerName(region, summonerName)
 summonerID = name_JSON['id']
+accountID = name_JSON['accountId']
+
+# find list of player's matches to extract data from
+matchlist_JSON = requestMatchList(region, accountID)
+matchId = []
+gameTime = []
+kda = []
+dpm = []
+vision = []
+gpm = []
+cs = []
+
+for matchNo in range(99):
+    matchId.append(matchlist_JSON['matches'][matchNo]['gameId'])
+    matchID = str(matchId[matchNo])
+    match_JSON = requestMatchInfo(region, matchID)
+    
+
+    # find correct participant(player) in single match
+    for pNo in range(9):
+        participantName = match_JSON['participantIdentities'][pNo]['player']['summonerName']
+        participantName = str(participantName)
+        if (participantName.lower() == summonerName.lower()):
+            partID = match_JSON['participantIdentities'][pNo]['participantId']
+            break
+
+    # find in-game statistics
+    for pNo in range(9):
+        if (match_JSON['participants'][pNo]['participantId'] == partID):
+            gameDuration = match_JSON['gameDuration']
+            kills = match_JSON['participants'][pNo]['stats']['kills']
+            assists = match_JSON['participants'][pNo]['stats']['assists']
+            deaths = match_JSON['participants'][pNo]['stats']['deaths']        
+            if(deaths == 0):
+                deaths = 1
+            totalDamageDealtToChampions = match_JSON['participants'][pNo]['stats']['totalDamageDealtToChampions']
+            visionScore = match_JSON['participants'][pNo]['stats']['visionScore']
+            goldEarned = match_JSON['participants'][pNo]['stats']['goldEarned']
+            totalMinionsKilled = match_JSON['participants'][pNo]['stats']['totalMinionsKilled']
+            neutralMinionsKilled = match_JSON['participants'][pNo]['stats']['neutralMinionsKilled']
+    
+    #minutes.append(int(gameDuration / 60))
+    minutes = int(gameDuration / 60)
+    seconds = gameDuration % 60
+    gameTime = str(minutes) + ':' + str(seconds)
+    gameTime = str(gameTime)
+    kda.append(round((kills + assists) / deaths, 2))
+    dpm.append(round(totalDamageDealtToChampions/gameDuration/60, 2))
+    vision.append(visionScore)
+    gpm.append(round(goldEarned/gameDuration/60, 2))
+    cs.append(totalMinionsKilled + neutralMinionsKilled)
+
+    print(kda)
+
+
 # total account game mastery
 summonerMastery = requestSummonerMastery(region, summonerID)
-
-accountID = name_JSON['accountId']
-matchlist_JSON = requestMatchList(region, accountID)
-matchId = matchlist_JSON['matches'][0]['gameId']
-matchID = str(matchId)
-match_JSON = requestMatchInfo(region, matchID)
-
-for pNo in range(10):
-    participantName = match_JSON['participantIdentities'][pNo]['player']['summonerName']
-    participantName = str(participantName)
-    #print("pName:",participantName)
-    if (participantName.lower() == summonerName.lower()):
-        partID = match_JSON['participantIdentities'][pNo]['participantId']
-        break
-#print("participant ID:", partID)
-
-for pNo in range(10):
-    if (match_JSON['participants'][pNo]['participantId'] == partID):
-        gameDuration = match_JSON['gameDuration']
-        kills = match_JSON['participants'][pNo]['stats']['kills']
-        assists = match_JSON['participants'][pNo]['stats']['assists']
-        deaths = match_JSON['participants'][pNo]['stats']['deaths']        
-        totalDamageDealtToChampions = match_JSON['participants'][pNo]['stats']['totalDamageDealtToChampions']
-        visionScore = match_JSON['participants'][pNo]['stats']['visionScore']
-        goldEarned = match_JSON['participants'][pNo]['stats']['goldEarned']
-        totalMinionsKilled = match_JSON['participants'][pNo]['stats']['totalMinionsKilled']
-        neutralMinionsKilled = match_JSON['participants'][pNo]['stats']['neutralMinionsKilled']
-
-# important in-game stats
-kda = (kills + assists)/deaths
-minutes = int(gameDuration / 60)
-kda = round((kills + assists)/deaths, 2)
-dpm = round(totalDamageDealtToChampions/minutes, 2)
-vision = visionScore
-gpm = round(goldEarned/minutes, 2)
-cs = totalMinionsKilled + neutralMinionsKilled
-
-#championId = requestMatchInfo['championId']
-
-#print(summonerID)
 print("Total Account Mastery:", summonerMastery)
 
 
 #pip the poro id: IS6jAFiLaj6yy3xcSpyIDpTMRvFEG6Vh3ph-I7RVhdyV7_M
     #my acct
-    #90
-    # acct id: fL_4VTvcp3LYbm9S1xQ78FFtZVG_VdWAQgnXeuMROWycN8s
-    # recent gameId: 3402933367
-'''
-Steps to finding game scores
-def-> matchlist by acct
-get "gameId"
-
-def-> match by matchid
-match player id
-
-calc kda = (kills + assists) / deaths
-"gameDuration" (in seconds)
-"totalDamageDealtToChampions"
-"visionScore"
-"goldEarned"
-"totalMinionsKilled"
-"neutralMinionsKilled"
-
-
-#should probably add player acct rank
-'''
-    
+    #mastery: 90
 #doublelift id: XV7kJbSAgcQO_JCx8EWs7grADevXPzUlR9QBV6oMvqpjwIg
     #famous pro
-    #302
+    #mastery: 302
 #c9 zvennn  id: RT0KducVYCbEwdnUrfQg5c1sh2LzIS2vXYuff336wrMeOo4
     #rank 1
-    #315
+    #mastery: 315
 #tl tactical id: t5G8-JWkv9AvJqPIApk-nxiH0hd2kUZ1lnT_lTlKEvtXNpvM
     #rank 3
-    #290
+    #mastery: 290
 #c9 zven id: WlP8-ALvIffYzMgcSLoA2Cj6zXqNtKXw24GEmEDe3azcFXc
     #rank 5
-    #349
+    #mastery: 349
