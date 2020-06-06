e5603be91c2467d15d30e0627d801fc14462e5bd
Jason Hur <jhur008@ucr.edu>
2020-06-06 15:38:10 -0700

    Able to plot a comparison between 2 accts.

--- riot_api.py
+++ riot_api.py
@@ -22,30 +22,40 @@ def requestMatchList(region, accountID):
 def requestMatchInfo(region, matchID):
     URL = 'https://' + region + '.api.riotgames.com/lol/match/v4/matches/' + matchID + '?api_key=' + api_key
     response = requests.get(URL)
-    #if 'URL' not in response
     return response.json()
 
 def plot_bar_kda():
-    # this is for plotting purpose
     index = np.arange(len(kda))
-    plt.bar(index, kda)
+    width = 0.3
+    #fig = plt.figure()
+    #ax = fig.add_subplot(111) #??
+    #rect1 = ax.bar(index, kda, width, color = 'b')
+    
+    index2 = np.arange(len(kda2))
+    rect1 = plt.bar(index, kda, width, color='b')
+    rect2 = plt.bar(index2 + width, kda2, width, color='r')
     plt.xlabel('games', fontsize=10)
     plt.ylabel('KDA score', fontsize=10)
-    plt.title('Avg KillDeathAssist Ratio over ' + str(matchCounter) + ' games: ' + str(avg_kda))
+    plt.title('Avg KillDeathAssist Ratio over ' + str(matchCounter) + ' games')
+    plt.legend( (rect1[0], rect2[0]), (summonerName + ' (' + str(avg_kda) + ')'\
+                                       , summonerName2 + ' (' + str(avg_kda2) + ')') )
     plt.show()
 
 def plot_bar_dpm():
     index = np.arange(len(dpm))
-    plt.bar(index, dpm)
+    index2 = np.arange(len(dpm2))
+    plt.bar(index, dpm, col='b')
+    plt.bar(index2, dpm2, col='r')
     plt.xlabel('games', fontsize=10)
     plt.ylabel('DPM score', fontsize=10)
     plt.title('Average DamagePerMin over ' + str(matchCounter) + ' games: ' + str(avg_dpm))
     plt.show()
 
 def plot_bar_cspm():
-    # this is for plotting purpose
     index = np.arange(len(cspm))
-    plt.bar(index, cspm)
+    index2 = np.arange(len(cspm2))
+    plt.bar(index, cspm, col='b')
+    plt.bar(index2, cspm2, col='r')
     plt.xlabel('games', fontsize=10)
     plt.ylabel('CSPM', fontsize=10)
     plt.title('Average CreepScorePerMin over ' + str(matchCounter) + ' games: ' + str(avg_cspm))
@@ -53,7 +63,9 @@ def plot_bar_cspm():
 
 def plot_bar_gpm():
     index = np.arange(len(gpm))
-    plt.bar(index, gpm)
+    index2 = np.arange(len(gpm2))
+    plt.bar(index, gpm, col='b')
+    plt.bar(index2, gpm2, col='r')
     plt.xlabel('games', fontsize=10)
     plt.ylabel('GPM score', fontsize=10)
     plt.title('Average GoldPerMin over ' + str(matchCounter) + ' games: ' + str(avg_gpm))
@@ -61,7 +73,9 @@ def plot_bar_gpm():
 
 def plot_bar_vision():
     index = np.arange(len(vision))
-    plt.bar(index, vision)
+    index2 = np.arange(len(vision2))
+    plt.bar(index, vision, col='b')
+    plt.bar(index2, vision2, col='r')
     plt.xlabel('games', fontsize=10)
     plt.ylabel('Vision score', fontsize=10)
     plt.title('Average Vision Score over ' + str(matchCounter) + ' games: ' + str(avg_vision))
@@ -69,19 +83,24 @@ def plot_bar_vision():
 
 # get region and user info
 region = (str)(input('Type in a region: '))
-summonerName = (str)(input('Type in a summoner name: '))
+summonerName = (str)(input('Type in your summoner name: '))
+summonerName2 = (str)(input('Type in a pro summoner name: '))
 
 # find information about user from in-game(public) name
 name_JSON = requestSummonerName(region, summonerName)
+name_JSON2 = requestSummonerName(region, summonerName2)
 
 summonerID = name_JSON['id']
 accountID = name_JSON["accountId"]
 summonerMastery = requestSummonerMastery(region, summonerID)
+summonerID2 = name_JSON2['id']
+accountID2 = name_JSON2["accountId"]
+summonerMastery2 = requestSummonerMastery(region, summonerID2)
 
 # find list of player's matches to extract data from
 matchlist_JSON = requestMatchList(region, accountID)
 matchId = []
-gameTime = []
+#gameTime = []
 kda = []
 dpm = []
 vision = []
@@ -89,18 +108,22 @@ gpm = []
 cspm = []
 matchCounter = 0
 for matchNo in range(100):
-    matchId.append(matchlist_JSON['matches'][matchNo]['gameId'])
-    matchID = str(matchId[matchNo])
-    match_JSON = requestMatchInfo(region, matchID)
+    # We only want to append relevant Summoner's Rift Games. queue: 400, 420
+    if matchlist_JSON['matches'][matchNo]['queue'] == 400 \
+    or matchlist_JSON['matches'][matchNo]['queue'] == 420:
+        matchId.append(matchlist_JSON['matches'][matchCounter]['gameId'])
+        matchID = str(matchId[matchCounter])
+        match_JSON = requestMatchInfo(region, matchID)
+        matchCounter += 1
     
-    if len(match_JSON) > 5:
-        # find correct participant(player) in single match
-        for pNo in range(10):
-            participantName = match_JSON['participantIdentities'][pNo]['player']['summonerName']
-            participantName = str(participantName)
-            if (participantName.lower() == summonerName.lower()):
-                partID = match_JSON['participantIdentities'][pNo]['participantId']
-                break
+        if len(match_JSON) > 5:
+            # find correct participant(player) in single match
+            for pNo in range(10):
+                participantName = match_JSON['participantIdentities'][pNo]['player']['summonerName']
+                participantName = str(participantName)
+                if (participantName.lower() == summonerName.lower()):
+                    partID = match_JSON['participantIdentities'][pNo]['participantId']
+                    break
 
         # find in-game statistics
         for pNo in range(10):
@@ -128,22 +151,83 @@ for matchNo in range(100):
         gpm.append(round(goldEarned/gameDuration*60, 2))
         cs = totalMinionsKilled + neutralMinionsKilled
         cspm.append(round(cs/gameDuration*60, 2))
-        matchCounter += 1
-        
+        #matchCounter += 1
+        # we only need data for 30 most recent relevant matches
+        if matchCounter == 30:
+            break
 
-kda_scores = [0, 0.5, 1.0, 1.5, 2.0, 2.5, 3.0, 3.5, 4.0, 4.5, 5.0, 5.5, \
-    6.0, 6.5, 7.0, 7.5, 8.0, 8.5, 9.0, 9.5, 10.0]
 avg_kda = round(sum(kda)/matchCounter, 2)
 avg_dpm = round(sum(dpm)/matchCounter, 2)
 avg_cspm = round(sum(cspm)/matchCounter, 2)
 avg_gpm = round(sum(gpm)/matchCounter, 2)
 avg_vision = int(sum(vision)/matchCounter)
 
+
+
+matchlist_JSON2 = requestMatchList(region, accountID2)
+matchId2 = []
+kda2 = []
+dpm2 = []
+vision2 = []
+gpm2 = []
+cspm2 = []
+matchCounter2 = 0
+for matchNo in range(100):
+    if matchlist_JSON2['matches'][matchNo]['queue'] == 400 \
+    or matchlist_JSON2['matches'][matchNo]['queue'] == 420:
+        matchId2.append(matchlist_JSON2['matches'][matchCounter2]['gameId'])
+        matchID2 = str(matchId2[matchCounter2])
+        match_JSON2 = requestMatchInfo(region, matchID2)
+        matchCounter2 += 1
+    
+        if len(match_JSON2) > 5:
+            for pNo in range(10):
+                participantName2 = match_JSON2['participantIdentities'][pNo]['player']['summonerName']
+                participantName2 = str(participantName2)
+                if (participantName2.lower() == summonerName2.lower()):
+                    partID2 = match_JSON2['participantIdentities'][pNo]['participantId']
+                    break
+
+        # find in-game statistics
+        for pNo in range(10):
+            if (match_JSON2['participants'][pNo]['participantId'] == partID2):
+                gameDuration = match_JSON2['gameDuration']
+                kills = match_JSON2['participants'][pNo]['stats']['kills']
+                assists = match_JSON2['participants'][pNo]['stats']['assists']
+                deaths = match_JSON2['participants'][pNo]['stats']['deaths']        
+                if(deaths == 0):
+                    deaths = 1
+                totalDamageDealtToChampions = match_JSON2['participants'][pNo]['stats']['totalDamageDealtToChampions']
+                visionScore = match_JSON2['participants'][pNo]['stats']['visionScore']
+                goldEarned = match_JSON2['participants'][pNo]['stats']['goldEarned']
+                totalMinionsKilled = match_JSON2['participants'][pNo]['stats']['totalMinionsKilled']
+                neutralMinionsKilled = match_JSON2['participants'][pNo]['stats']['neutralMinionsKilled']
+        
+        kda2.append(round((kills + assists) / deaths, 2))
+        dpm2.append(round(totalDamageDealtToChampions/gameDuration*60, 2))
+        vision2.append(visionScore)
+        gpm2.append(round(goldEarned/gameDuration*60, 2))
+        cs = totalMinionsKilled + neutralMinionsKilled
+        cspm2.append(round(cs/gameDuration*60, 2))
+        if matchCounter2 == 30:# or matchCounter2 == matchCounter:
+            break
+
+avg_kda2 = round(sum(kda2)/matchCounter, 2)
+avg_dpm2 = round(sum(dpm2)/matchCounter, 2)
+avg_cspm2 = round(sum(cspm2)/matchCounter, 2)
+avg_gpm2 = round(sum(gpm2)/matchCounter, 2)
+avg_vision2 = int(sum(vision2)/matchCounter)
+
+
+
+
+
+
 plot_bar_kda()
-plot_bar_dpm()
-plot_bar_cspm()
-plot_bar_gpm()
-plot_bar_vision()
+#plot_bar_dpm()
+#plot_bar_cspm()
+#plot_bar_gpm()
+#plot_bar_vision()
 
 #print("avg kda: ", round(avg_kda, 2))
 #print("Total Account Mastery:", summonerMastery)
