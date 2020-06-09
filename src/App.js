7a2a5703bc4f47dd6ceb49c2af10132a1a8c1094
Edward Dickhoff <32476243+edick007@users.noreply.github.com>
2020-06-08 17:04:05 -0700

    set up firebase for realtime if needed

--- src/App.js
+++ src/App.js
@@ -4,12 +4,33 @@ import './App.css';
 
 import Header from './components/Header';
 import Content from './components/Content';
+import firebase from 'firebase';
+
 
 export default class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      speed: 11
+    };
+  }
+
+  componentDidMount() { 
+    const rootRef = firebase.database().ref();
+    const speedRef = rootRef.child('speed');
+    speedRef.on('value', snap => {
+      this.setState = ({
+        speed: snap.val()
+      });
+    });
+  }
+
   render() {   
     return (
       <div className="App">
         <Header/>
+        <h1>{this.state.speed}</h1>
         <br />
         <Content/>
      </div>
--- src/index.js
+++ src/index.js
@@ -4,6 +4,19 @@ import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import firebase from 'firebase';
+
+var config = {
+  apiKey: "ask edward",
+  authDomain: "proward-6b420.firebaseapp.com",
+  databaseURL: "https://proward-6b420.firebaseio.com",
+  projectId: "proward-6b420",
+  storageBucket: "proward-6b420.appspot.com",
+  messagingSenderId: "192314226070",
+  appId: "1:192314226070:web:8226c57f3bfae7c62bd9b7",
+  measurementId: "G-WVW284HWVL"
+}
+firebase.initializeApp(config);
 
 ReactDOM.render((
     <BrowserRouter>
@@ -15,3 +28,5 @@ ReactDOM.render((
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
 serviceWorker.unregister();
+ 
+export default firebase
